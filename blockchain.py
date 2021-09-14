fe0c27ee854d2494eaee55d9e69c650f16a30355
charly clar <charlyclar.pro@gmail.com>
2021-09-14 22:37:42 +0200

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Block();
+from hashlib import sha256
+
+class Block():
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = "0" * 64
+
+    def __int__(self,data,number=0):
+        self.data = data
+        self.number = number
+
+class Blockchain():
     pass
+
+
+
+
+
+
+
+def main():
+    block = Block("hello world",1)
+
+if __name__=='__name__':
+    main()
+
 
