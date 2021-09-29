6a90904ac92d4ce663663148bf977285d943cc4a
charly clar <charlyclar.pro@gmail.com>
2021-09-29 22:47:01 +0200

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -12,13 +12,9 @@ def updatehash(*args):
     h.update(hashing_text.encode('utf-8'))
     return h.hexdigest()
 
-#print(updatehash("hello world","christelle"))
-
-
 class Block():
     data = None
-    hash = None
-    nonce = 0
+    hash = None; nonce = 0
     previous_hash = "0" * 64
 
     def __init__(self,data,number=0):
@@ -36,8 +32,7 @@ class Block():
 
     
     def __str__(self):
-        return str("Blocks#: %s\nHash: %s\nPrevious:" + 
-            "%s\nData: %s\nNonce: %s\n" %(
+        return str("Blocks#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(
             self.number,
             self.hash(),
             self.previous_hash, 
@@ -54,22 +49,18 @@ class Blockchain():
         self.chain = chain
     
     def add(self, block):
-        self.chain.append({
-            'hash': block.hash(), 
-            'previous': block.previous_hash, 
-            'number': block.number, 
-            'data': block.data, 
-            'nonce': block.nonce
-        })
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
     
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get('hash')
-        except IndexError:
-            pass
+            block.previous_hash = self.chain[-1].hash()
+        except IndexError: pass
 
         while True: 
-            if block.hash()[:4] == "0" * self.difficulty:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
                 self.add(block); break
             else:
                 block.nonce += 1
@@ -80,6 +71,7 @@ def main():
     database = ["hello wolrd", "what's up", "hello", "bye"]
 
     num = 0
+
     for data in database:
         num += 1
         blockchain.mine(Block(data,num))
