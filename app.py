eefecfcfeca5d6870fcc59d104b91051ed66619d
charly clar <charlyclar.pro@gmail.com>
2021-10-10 22:23:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import Flask, render_template, flash, redirect, url_for, session, req
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 
-
+ # from passwords import _mysql_password
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '' #_mysql_password
 app.config['MYSQL_DB'] = 'crypto'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
