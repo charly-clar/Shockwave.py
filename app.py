724c62c0c4feaa1529bec305b597c9f5bf0853d2
charly clar <charlyclar.pro@gmail.com>
2021-10-10 23:33:00 +0200

    commit

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return "hello world!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.secret_key = 'secret123'
---
+++ sqlhelpers.py
@@ -0,0 +1,90 @@
+from app import mysql, session
+
+
+class Table():
+    #specify the table name and columns
+    #EXAMPLE table:
+    #               blockchain
+    # number    hash    previous   data    nonce
+    # -data-   -data-    -data-   -data-  -data-
+    #
+    #EXAMPLE initialization: ...Table("blockchain", "number", "hash", "previous", "data", "nonce")
+    def _init_(self, table_name, *args):
+        self.table = table_name
+        self.columns = "(%s)" %",".join(args)
+
+        #if table does not already exist, create it.
+        if isnewtable(table_name):
+            cur = mysql.connection.cursor()
+            cur.execute("CREATE TABLE %s%s" %(self.table, self.columns))
+            cur.close()
+    
+        #get all the values from the table
+    def getall(self):
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM %s" %self.table)
+        data = cur.fetchall(); return data
+
+    #get one value from the table based on a column's data
+    #EXAMPLE using blockchain: ...getone("hash","00003f73gh93...")
+    def getone(self, search, value):
+        data = {}; cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM %s WHERE %s = \"%s\"" %(self.table, search, value))
+        if result > 0: data = cur.fetchone()
+        cur.close(); return data
+
+    #delete a value from the table based on column's data
+    def deleteone(self, search, value):
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE from %s where %s = \"%s\"" %(self.table, search, value))
+        mysql.connection.commit(); cur.close()
+
+    #delete all values from the table.
+    def deleteall(self):
+        self.drop() #remove table and recreate
+        self.__init__(self.table, *self.columnsList)
+
+    #remove table from mysql
+    def drop(self):
+        cur = mysql.connection.cursor()
+        cur.execute("DROP TABLE %s" %self.table)
+        cur.close()
+
+    #insert values into the table
+    def insert(self, *args):
+        data = ""
+        for arg in args: #convert data into string mysql format
+            data += "\"%s\"," %(arg)
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO %s%s VALUES(%s)" %(self.table, self.columns, data[:len(data)-1]))
+        mysql.connection.commit()
+        cur.close()
+
+#execute mysql code from python
+def sql_raw(execution):
+    cur = mysql.connection.cursor()
+    cur.execute(execution)
+    mysql.connection.commit()
+    cur.close()
+
+#check if table already exists
+def isnewtable(tableName):
+    cur = mysql.connection.cursor()
+
+    try: #attempt to get data from table
+        result = cur.execute("SELECT * from %s" %tableName)
+        cur.close()
+    except:
+        return True
+    else:
+        return False
+
+#check if user already exists
+def isnewuser(username):
+    #access the users table and get all values from column "username"
+    users = Table("users", "name", "email", "username", "password")
+    data = users.getall()
+    usernames = [user.get('username') for user in data]
+
+    return False if username in usernames else True
