f211aa62ef0c2b1b012f952674587a0cab8fb31f
charly clar <charlyclar.pro@gmail.com>
2021-10-12 00:08:18 +0200

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#import flask dependencies for web GUI
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 
+from sqlhelpers import *
  # from passwords import _mysql_password
 
 app = Flask(__name__)
@@ -13,13 +15,16 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '' #_mysql_password
 app.config['MYSQL_DB'] = 'crypto'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-
+#initialize mysql
 mysql = MySQL(app)
 
 @app.route("/")
 def index():
+    users = Table("users","name","email","username","passeword")
+    users.insert("John Doe","jd@gmail.com","johndoe","hash")
+    #users.drop()
     return render_template('index.html')
 
 if __name__ == "__main__":
--- sqlhelpers.py
+++ sqlhelpers.py
@@ -2,58 +2,51 @@ from app import mysql, session
 
 
 class Table():
-    #specify the table name and columns
-    #EXAMPLE table:
-    #               blockchain
-    # number    hash    previous   data    nonce
-    # -data-   -data-    -data-   -data-  -data-
-    #
-    #EXAMPLE initialization: ...Table("blockchain", "number", "hash", "previous", "data", "nonce")
     def _init_(self, table_name, *args):
         self.table = table_name
         self.columns = "(%s)" %",".join(args)
+        self.columnsLists = args
 
-        #if table does not already exist, create it.
         if isnewtable(table_name):
-            cur = mysql.connection.cursor()
-            cur.execute("CREATE TABLE %s%s" %(self.table, self.columns))
+            create_data = ""
+            for column in self.columnsList:
+                create_data += "%s varchar(100)," %column
+
+            cur = mysql.connection.cursor() #create the table
+            print("CREATE TABLE %s(%s)" %(self.table, create_data[:len(create_data)-1]))
+            cur.execute("CREATE TABLE %s(%s)" %(self.table, create_data[:len(create_data)-1]))
             cur.close()
+            
+
     
-        #get all the values from the table
+       
     def getall(self):
         cur = mysql.connection.cursor()
         result = cur.execute("SELECT * FROM %s" %self.table)
         data = cur.fetchall(); return data
 
-    #get one value from the table based on a column's data
-    #EXAMPLE using blockchain: ...getone("hash","00003f73gh93...")
+ 
     def getone(self, search, value):
         data = {}; cur = mysql.connection.cursor()
         result = cur.execute("SELECT * FROM %s WHERE %s = \"%s\"" %(self.table, search, value))
         if result > 0: data = cur.fetchone()
         cur.close(); return data
 
-    #delete a value from the table based on column's data
+   
     def deleteone(self, search, value):
         cur = mysql.connection.cursor()
         cur.execute("DELETE from %s where %s = \"%s\"" %(self.table, search, value))
         mysql.connection.commit(); cur.close()
 
-    #delete all values from the table.
-    def deleteall(self):
-        self.drop() #remove table and recreate
-        self.__init__(self.table, *self.columnsList)
-
-    #remove table from mysql
+   
     def drop(self):
         cur = mysql.connection.cursor()
         cur.execute("DROP TABLE %s" %self.table)
         cur.close()
 
-    #insert values into the table
     def insert(self, *args):
         data = ""
-        for arg in args: #convert data into string mysql format
+        for arg in args: 
             data += "\"%s\"," %(arg)
 
         cur = mysql.connection.cursor()
@@ -61,18 +54,18 @@ class Table():
         mysql.connection.commit()
         cur.close()
 
-#execute mysql code from python
+
 def sql_raw(execution):
     cur = mysql.connection.cursor()
     cur.execute(execution)
     mysql.connection.commit()
     cur.close()
 
-#check if table already exists
+
 def isnewtable(tableName):
     cur = mysql.connection.cursor()
 
-    try: #attempt to get data from table
+    try: 
         result = cur.execute("SELECT * from %s" %tableName)
         cur.close()
     except:
@@ -80,9 +73,9 @@ def isnewtable(tableName):
     else:
         return False
 
-#check if user already exists
+
 def isnewuser(username):
-    #access the users table and get all values from column "username"
+  
     users = Table("users", "name", "email", "username", "password")
     data = users.getall()
     usernames = [user.get('username') for user in data]
